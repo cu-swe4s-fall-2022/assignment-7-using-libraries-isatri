0964d82223d38d20073d93f014d20b9c7ca54ef9
itristan <itristan@cu-genvpn-tcom-10.180.154.161.int.colorado.edu>
2022-10-21 03:41:27 -0600

    developed write_matrix_to_file in data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -2,6 +2,8 @@
 """Functions for processing data in Assignment 7
 
     * get_random_matrix - returns an NxM Numpy matrix of random (0,1] values
+	* get_file_dimensions - returns the dimensions of data from a read CSV file
+	* write_matrix_to_file - generates random NxM matrix and saves to CSV file
 
 """
 import numpy as np
@@ -23,11 +25,11 @@ def get_random_matrix(num_rows, num_columns):
         An NxM random matrix with values between (0,1]
 
     """
-	# Catch inappropriate input types and values
+	# Catch inappropriate data types/values
 	if type(num_rows) != int or type(num_columns) != int:
-		raise TypeError('Matrix rows and columns must be integers.')
+		raise TypeError('Matrix dimensions must be integers.')
 	elif num_rows <= 0 or num_columns <= 0:
-		raise ValueError('Matrix rows and columns must be > 0.')
+		raise ValueError('Matrix dimensions must be > 0.')
 
 	return np.random.rand(num_rows, num_columns)
 
@@ -43,7 +45,7 @@ def get_file_dimensions(file_name):
 
     Returns
     -------
-    file.shape : tuple
+    file.shape : tuple of int
         The dimensions of the tabular data in the CSV file
 
     """
@@ -55,4 +57,24 @@ def get_file_dimensions(file_name):
 
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+	"""Creates an NxM Numpy matrix of random (0,1] values and saves it to a
+	CSV file with a user inputted name.
+
+    Parameters
+    ----------
+    num_rows : int
+        Number of rows for the generated random matrix
+    num_columns : int
+        Number of colums for the generated random matrix
+    file_name : str
+		The name of the CSV file to save
+
+    Returns
+    -------
+
+    """
+	if type(file_name) != str:  # Catch inappropriate input types
+		raise TypeError('File name must be a string of characters.')
+
+	random_matrix = get_random_matrix(num_rows, num_columns)
+	np.savetxt(file_name, random_matrix, delimiter=",")
