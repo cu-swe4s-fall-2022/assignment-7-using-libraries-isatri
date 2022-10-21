1466ca3b5daaa5c7a06dbaa3fd2221404ef03ea1
itristan <itristan@cu-genvpn-tcom-10.180.154.161.int.colorado.edu>
2022-10-21 04:46:55 -0600

    checked files with pycodestyle

--- plotter.py
+++ plotter.py
@@ -6,6 +6,7 @@ sepal_width per iris_species.
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def main():
     data = pd.read_csv('iris.data', sep=',', header=None)
     data.columns = ['sepal_width',
@@ -19,16 +20,16 @@ def main():
     fig3, ax3 = plt.subplots(1, 2, figsize=(9, 4))  # combination
 
     for axs in [ax1, ax3[0]]:
-        axs.boxplot(data.iloc[:,:4])
-        axs.set(ylabel = '[cm]',
-               xticklabels = data.columns[:4])
+        axs.boxplot(data.iloc[:, :4])
+        axs.set(ylabel='[cm]',
+                xticklabels=data.columns[:4])
 
     for axs in [ax2, ax3[1]]:
         for species_name in data['iris_species'].unique()[::-1]:
             iris_subset = data[data['iris_species'] == species_name]
             axs.scatter(iris_subset['sepal_length'],
-                       iris_subset['sepal_width'],
-                       label=species_name)
+                        iris_subset['sepal_width'],
+                        label=species_name)
         axs.set(xlabel='sepal_length [cm]',
                 ylabel='sepal_width [cm]')
         axs.legend(fontsize=8)
@@ -43,6 +44,7 @@ def main():
     fig1.savefig('iris_boxplot.png')
     fig2.savefig('petal_width_v_length_scatter.png')
     fig3.savefig('multi_panel_figure.png')
+
 
 if __name__ == '__main__':
     main()
